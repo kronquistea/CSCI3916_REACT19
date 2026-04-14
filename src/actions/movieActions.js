5894b602cfc9d5452535ec281714cfd2daedba07
Erik Kronquist <erik.kronquist@gmail.com>
2026-04-14 10:16:42 -0600

    Include proper api route

--- src/actions/movieActions.js
+++ src/actions/movieActions.js
@@ -26,7 +26,7 @@ function movieSet(movie) {
 export const addReview = (movieId, review) => async (dispatch) => {
   try {
     console.log(review)
-    await fetch(`/api/movies/${movieId}/reviews`, {
+    await fetch(`${env.REACT_APP_API_URL}/movies/${movieId}/reviews`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
