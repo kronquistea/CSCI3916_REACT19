4f2bdd059ae21ba1c32c9035c96b2c813e1bd24b
Erik Kronquist <erik.kronquist@gmail.com>
2026-04-14 10:16:11 -0600

    Fix addReview to include /api in route

--- src/actions/movieActions.js
+++ src/actions/movieActions.js
@@ -26,7 +26,7 @@ function movieSet(movie) {
 export const addReview = (movieId, review) => async (dispatch) => {
   try {
     console.log(review)
-    await fetch(`/movies/${movieId}/reviews`, {
+    await fetch(`/api/movies/${movieId}/reviews`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
